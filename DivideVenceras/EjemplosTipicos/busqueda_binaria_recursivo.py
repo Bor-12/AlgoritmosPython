9dcbe16215878ab70b77ed2d1fafcdf02e3087db
Borja <borjagonzare@gmail.com>
2025-05-14 12:55:17 +0200

    actualizacion busqueda binaria

--- DivideVenceras/EjemplosTipicos/busqueda_binaria_recursivo.py
+++ DivideVenceras/EjemplosTipicos/busqueda_binaria_recursivo.py
@@ -18,7 +18,7 @@ target = 30
 print(busqueda_binaria_recursivo(lista, target))
 
 def busqueda_binaria_recursivo_indice(numeros, valor_buscado, izquierda = 0, derecha = None):
-    """Devuleve el indice donde se encuentra el numero buscado, si no lo encuentra devuelve -1 o la logitud de la lista dada"""
+    """Devuleve el indice donde se encuentra el numero buscado, si no lo encuentra devuelve -1 o la logitud de la lista  - 1"""
     if derecha == None:
         derecha = len(numeros) - 1
     if izquierda > derecha:
@@ -41,3 +41,40 @@ target = 17
 print(busqueda_binaria_recursivo_indice(lista, target))
 target = 88
 print(busqueda_binaria_recursivo_indice(lista, target))
+#se puede utilizar por ejemplo para saber donde insertar un valor en una lista ordenada, devolvermos la posicion donde se deveria de insertar
+def busqueda_binaria_recursivo_insertar(numeros, valor, izquierda=0, derecha=None):
+    if derecha is None:
+        derecha = len(numeros) - 1
+
+    if izquierda > derecha:
+        return izquierda
+
+    medio = (izquierda + derecha) // 2
+
+    if numeros[medio] >= valor:
+        return busqueda_binaria_recursivo_insertar(numeros, valor, izquierda, medio - 1)
+    else:
+        return busqueda_binaria_recursivo_insertar(numeros, valor, medio + 1, derecha)
+
+def busqueda_binaria_recursivo_insertar(numeros, valor, izquierda=0, derecha=None):
+    if derecha is None:
+        derecha = len(numeros) - 1
+
+    if izquierda > derecha:
+        return izquierda
+
+    medio = (izquierda + derecha) // 2
+
+    if numeros[medio] >= valor:
+        return busqueda_binaria_recursivo_insertar(numeros, valor, izquierda, medio - 1)
+    else:
+        return busqueda_binaria_recursivo_insertar(numeros, valor, medio + 1, derecha)
+
+
+lista = [1, 3, 5, 6, 12, 15, 66]
+test_values = [5, 1, 4, 6, 7, 15, 30, 70]
+
+print("Insertar en lista:", lista)
+for val in test_values:
+    idx = busqueda_binaria_recursivo_insertar(lista, val)
+    print(f"Insertar {val} en posición {idx}")
