36c44a3d3e996ec77c4d2e2602e39e8077a33a13
Borja <borjagonzare@gmail.com>
2025-05-15 19:44:03 +0200

    suma subconjuntos  actualizada con una  funcion boolena

--- Backtracking/EjemplosTipicos/suma_subconjuntos.py
+++ Backtracking/EjemplosTipicos/suma_subconjuntos.py
@@ -1,4 +1,3 @@
-
 def suma_subconjuntos(elementos, target):
     sol = [0] * len(elementos)
     suma_subconjuntos_recursivo(0, sol, 0,elementos,target)
@@ -25,7 +24,7 @@ print()
 
 def suma_subconjuntos2(elementos, target):
     sol = [0] * len(elementos)
-    suma_subconjuntos_recursivo(0, sol, 0,elementos,target)
+    suma_subconjuntos_recursivo2(0, sol, 0,elementos,target)
 def mostrar_conjunto2(solucion, elementos):
     for i in range(len(elementos)):
         if solucion[i] == 1:
@@ -42,6 +41,26 @@ def suma_subconjuntos_recursivo2(i, sol, suma_actual,elementos,target):
                 suma_actual += k*elementos[i]
                 sol[i] = k
                 suma_subconjuntos_recursivo2(i + 1, sol, suma_actual, elementos, target)
-elementos = [0,1,2,3, 5, 6, 7, 9]
+elementos = [1,2,3, 5, 6, 7, 9]
 target = 13
 suma_subconjuntos2(elementos, target)
+
+#devuelve true si  hay alguna suma que de igual al target
+def suma_subconjuntos_recursivo_booleano(elementos, target):
+    return suma_subconjuntos_recursivo_booleano_recursivo(0, 0, elementos, target)
+
+def suma_subconjuntos_recursivo_booleano_recursivo(i, suma_actual, elementos, target):
+    if i == len(elementos):
+        return suma_actual == target
+    else:
+        for k in range(2):
+            nueva_suma = suma_actual + k * elementos[i]
+            if nueva_suma <= target:
+                if suma_subconjuntos_recursivo_booleano_recursivo(i + 1, nueva_suma, elementos, target):
+                    return True
+        return False
+
+elementos = [1,2,3, 5, 6, 7, 9]
+target = 13
+
+print(suma_subconjuntos_recursivo_booleano(elementos, target))
