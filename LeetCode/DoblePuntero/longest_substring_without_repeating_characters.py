573777629a4b8b178e9a0638a73c8f058f037dee
Borja <borjagonzare@gmail.com>
2025-05-30 11:22:50 +0200

    Leet code problema 3

---
+++ LeetCode/DoblePuntero/longest_substring_without_repeating_characters.py
@@ -0,0 +1,14 @@
+#Problema 3
+def longest_substring_without_repeating_characters(s: str) -> int:
+    inicio = 0
+    fin = 0
+    maximo = 0
+    caracteres_ocupados = set()
+    while fin < len(s):
+        while s[fin] in caracteres_ocupados:
+            caracteres_ocupados.remove(s[inicio])
+            inicio += 1
+        caracteres_ocupados.add(s[fin])
+        fin += 1
+        maximo = max(maximo, fin - inicio)
+    return maximo
